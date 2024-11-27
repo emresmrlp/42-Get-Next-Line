221dd02dcacf34d483befa8b6be862e9158cf8d7
Yunus Emre Can Sümeralp <39301904+emresmrlp@users.noreply.github.com>
2024-11-27 20:39:34 +0300

    Add files via upload

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: ysumeral <ysumeral@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/23 21:01:40 by ysumeral          #+#    #+#             */
-/*   Updated: 2024/11/24 10:30:25 by ysumeral         ###   ########.fr       */
+/*   Updated: 2024/11/27 17:06:10 by ysumeral         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,8 @@
 # include <unistd.h>
 
 char	*get_next_line(int fd);
-char	*ft_substr(char const *str, unsigned int start, size_t len);
 char	*ft_strjoin(char *s1, char *s2);
-char	*ft_strchr(const char *str, int c);
+char	*ft_strchr( char *str, int c);
 size_t	ft_strlen(const char *str);
 
 #endif
